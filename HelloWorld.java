69fbbfc02d5af0aad655c7e335937d75d9cdbc76
JxxD-02 <jxxd-02@DTWG-026.local>
2025-07-26 14:34:41 +0700

    Update HelloWorld.java with new code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("This is my first Java program and I can use Git to manage my code.");
     }
 }
