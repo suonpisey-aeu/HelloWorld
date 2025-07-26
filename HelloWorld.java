c2447b0f70061fc642334f91c9120107c5a7783d
suonpisey-aeu <suonpisey.aeu@gmail.com>
2025-07-26 14:36:24 +0700

    Update HelloWorld.java
    just test01

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is my first Java program and I can use Git to manage my code.");
+        System.out.println("This is my first Java program and I can use Git to manage my code----from here.");
     }
 }
